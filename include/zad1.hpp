df08d677892f846d4f1c60be06c369223424f681
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2021-01-15 07:27:05 +0000

    zadanie 1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,4 +6,23 @@
 #include <sstream>
 #include <string>
 
-// tutaj klasa Artysta
+class Artysta 
+{
+public:
+    Artysta() : pseudonim{"Anonim"}, liczba_sluchaczy{0} {}
+    Artysta(const std::string& p_, unsigned l_) : pseudonim{p_}, liczba_sluchaczy{l_} {}
+
+    void graj(std::ostream& stream) const
+    {
+
+        stream << pseudonim << ": " << liczba_sluchaczy;
+    }
+
+    const std::string& getPseudonim() const {return pseudonim;}
+    const unsigned getSluchacze() const {return liczba_sluchaczy;}
+
+
+private:
+    std::string pseudonim;
+    unsigned liczba_sluchaczy; 
+};
