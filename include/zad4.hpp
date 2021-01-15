f175230de7849ad161990cfec8e2a6662445502c
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2021-01-15 08:16:54 +0000

    zadanie 4

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -4,3 +4,14 @@
 #include <vector>
 
 // tutaj funkcja stworzZespol
+std::vector<Artysta> stworzZespol(int liczba, const std::string& nazwa)
+{
+    std::vector<Artysta> wektor;
+    if (nazwa.size() != liczba)
+        throw std::logic_error("zla liczba");
+    
+    for(int i = 1; i < liczba+1; i++)
+        wektor.push_back(Artysta{nazwa, static_cast<unsigned>(i)});
+    
+    return wektor;
+}
