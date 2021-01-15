6a340e1a3f5267c9afe4edb6f62e6f4a619a41fa
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2021-01-15 08:25:04 +0000

    zadanie 5

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -5,3 +5,8 @@
 #include <vector>
 
 // tutaj algorytm liczPopularnych(T begin, T end)
+template<typename T>
+unsigned liczPopularnych(T it1, T it2)
+{
+    return std::count_if(it1, it2, [](Artysta& a) {return a.getSluchacze() > 1000;})
+}
