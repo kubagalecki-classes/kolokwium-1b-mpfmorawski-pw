089566158fcedff051328ed7cc1f387f085878e3
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2021-01-15 07:50:10 +0000

    zadanie 3

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -3,3 +3,13 @@
 #include "zad1.hpp"
 
 //tutaj szablon ArtystaPlusInstrument
+template<typename T>
+class ArtystaPlusInstrument
+{
+public:
+    ArtystaPlusInstrument(const Artysta& a_, const T& i_) : a{a}, i{i} {}
+    unsigned koncert() {return i.liczbaUczestnikow(a.getSluchacze());}
+private:
+    Artysta a;
+    T i;
+};
